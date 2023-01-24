7d2add9c6b373cd1cbe30b8f72bbc7265b3d032f
Jonathan Doorn <jon@doorn.us>
2023-01-24 13:19:02 -0500

    wifi init is working

--- wifi_setup.py
+++ wifi_setup.py
@@ -8,15 +8,21 @@ def parse_at_output(_in: bytes):
 
 def write_flush_getline(port: serial.Serial, to_write: bytes) -> bytes :
     port.write(to_write)
-    port.flush()
-    return port.readline()
+    # port.flush()
+    # port.readline()
+    # port.flush()
+    # return port.readline()
+    return port.read_until(b'OK\r\n')
 
 def test_comport(port: serial.Serial):
-   assert write_flush_getline(port, b'AT\r\n') ==  b'OK\r\n'
+#    assert write_flush_getline(port, b'AT\r\n') ==  b'OK\r\n'
+    print(write_flush_getline(port, b'AT\r\n'))
 
 def set_soft_ap(port: serial.Serial):
     write_flush_getline(port, b'AT+CWMODE=2\r\n')
 
+def set_wifi_link(port, ssid, password):
+    write_flush_getline(port, b'AT+CWSAP="' + bytes(ssid, "ascii") + b'","' + bytes(password, "ascii") + b'",5,3\r\n')
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -25,3 +31,5 @@ if __name__ == "__main__":
     
     port = serial.Serial(comport, 115200)
     test_comport(port)
+    set_soft_ap(port)
+    set_wifi_link(port, "myap", "012345678")
