95c3c16b8eda7957e7a9b56a55ad5d9d623ec0a5
Fkepler5 <fkepler@purdue.edu>
2023-02-16 15:22:02 -0500

    Added Basic number functionality to keypad (tested)

--- uartlite/Core/Src/keypad.c
+++ uartlite/Core/Src/keypad.c
@@ -11,14 +11,32 @@
 
 
 void keypad(){
+	int count = 0;
 	LL_TIM_EnableCounter(TIM6);
 	spi_init_oled();
-	spi_display2("test");
+	spi_display1("Enter # of Spots");
+	char key;
 	for(;;) {
-		char key = get_keypress();
-		if (key == 'A')
-			spi_display2("10");
-		else if (key == 'B')
-    	   	spi_display2("20");
+		key = get_keypress();
+		if (key == '0')
+			spi_display2("0");
+		else if (key == '1')
+    	   	spi_display2("1");
+		else if (key == '2')
+			spi_display2("2");
+		else if (key == '3')
+			spi_display2("3");
+		else if (key == '4')
+			spi_display2("4");
+		else if (key == '5')
+			spi_display2("5");
+		else if (key == '6')
+			spi_display2("6");
+		else if (key == '7')
+			spi_display2("7");
+		else if (key == '8')
+			spi_display2("8");
+		else if (key == '9')
+			spi_display2("9");
 	}
 }
