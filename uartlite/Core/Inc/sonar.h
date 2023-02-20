9b086fba4bcdd99ab65428edbed4094d182a4719
Jonathan Doorn <jon@doorn.us>
2023-02-19 19:48:45 -0500

    added debounce code for proximity sensor

--- uartlite/Core/Inc/sonar.h
+++ uartlite/Core/Inc/sonar.h
@@ -9,6 +9,20 @@
 #define INC_SONAR_H_
 #include "main.h"
 
+#define SONAR_DEBOUNCE_WIDTH 5
+
+#define SONAR_THRESH_TRIG 10
+#define SONAR_THRESH_CLEAR 50
+#define SONAR_TRIGGERED 1
+
+typedef struct {
+
+	uint16_t buffer[SONAR_DEBOUNCE_WIDTH];
+	uint8_t buffer_idx;
+	uint8_t triggered;
+	uint8_t trigger_read;
+} sonar_debouncer_t;
+
 int sonar(void);
 
 
--- uartlite/Core/Src/sonar.c
+++ uartlite/Core/Src/sonar.c
@@ -8,6 +8,8 @@
 #include "asmutils.h"
 #include "ustimer.h"
 
+sonar_debouncer_t debounce_state = {0}; // need to set buffer to max val on init
+
 void us_enable(void) {
 	LL_GPIO_SetOutputPin( PROX_TRIG_GPIO_Port, PROX_TRIG_Pin);
 	return;
@@ -18,7 +20,56 @@ void us_disable(void) {
 	return;
 }
 
+void sonar_init() {
+	int i;
+	for (i = 0; i < SONAR_DEBOUNCE_WIDTH; i++) {
+		debounce_state[i] = UINT16_MAX;
+	}
+	debounce_state.buffer_idx = 0;
+	debounce_state.triggered=0;
+	debounce_state.trigger_read = 0;
+}
+
+uint8_t read_trigger_val() {
+	if (debounce_state.triggered && !debounce_state.trigger_read) {
+		debounce_state.trigger_read = SONAR_TRIGGERED;
+		return SONAR_TRIGGERED;
+	}
+	return !SONAR_TRIGGERED;
+}
+
+void debounce() {
+	int i;
+
+	// check all under threshold
+	if (debounce_state.trigger_read) {
+		for (i = 0; i < SONAR_DEBOUNCE_WIDTH; i++) {
+			if (debounce_state.buffer[i] >= SONAR_THRESH_CLEAR)
+				continue;
+			else
+				break;
+		}
+		if (i == SONAR_DEBOUNCE_WIDTH) {
+			debounce_state.triggered = 0;
+			debounce_state.trigger_read = 0;
+			return;
+		}
+	}
+	for (i = 0; i < SONAR_DEBOUNCE_WIDTH; i++) {
+		if (debounce_state.buffer[i] <= SONAR_THRESH_TRIG)
+			continue;
+		else
+			break;
+	}
+	if (i == SONAR_DEBOUNCE_WIDTH) {
+		debounce_state.triggered = 1;
+		return;
+	}
+}
+
 int sonar(void) {
+
+	int us_count;
 
 	us_enable();
 	nano_wait(10000);
@@ -34,5 +85,12 @@ int sonar(void) {
 	uscounter_start();
 	while( (LL_GPIO_IsInputPinSet(PROX_MEAS_GPIO_Port, PROX_MEAS_Pin)));
 	uscounter_stop();
-	return uscounter_get_count();
+
+	us_count = uscounter_get_count();
+	debounce_state.buffer[debounce_state.buffer_idx++] = us_count;
+	debounce_state.buffer_idx %= SONAR_DEBOUNCE_WIDTH;
+
+	debounce();
+
+	return us_count;
 }
